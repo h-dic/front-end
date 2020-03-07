bfce076ba4d94881c02d42d01171b6d880bc8992
Quarez Etienne <etienne.quarez@insa-rouen.fr>
2020-03-08 00:03:47 +0100

    deleted difference between closed and open expansion panel

--- src/components/ExpansionPanel.vue
+++ src/components/ExpansionPanel.vue
@@ -1,23 +1,8 @@
 <template>
   <v-expansion-panel>
     <v-expansion-panel-header>
-      <template v-slot:default="{ open }">
-        <v-container fill-height fluid class="pa-0">
-          <v-row no-gutters align="center">
-            {{ title }}
-            <v-col auto-grow class="text--secondary">
-              <v-fade-transition leave-absolute>
-                <span v-if="open">
-                  <slot name="openSubtext"></slot>
-                </span>
-                <span v-else>
-                  <slot name="closeSubtext"></slot>
-                </span>
-              </v-fade-transition>
-            </v-col>
-          </v-row>
-        </v-container>
-      </template>
+      {{ title }}
+      <slot name="closeSubtext"></slot>
     </v-expansion-panel-header>
     <v-expansion-panel-content>
       <slot name="select"></slot>
